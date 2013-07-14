902f980de9a6564958b0c8526f50407645928e5c
Michael Gurlitz <michael@gurlitz.org>
2013-07-14 19:18:01 +0000

    Also show privately shared tracks on the homepage.

--- app/models/soundcloud/user.js
+++ app/models/soundcloud/user.js
@@ -18,9 +18,12 @@ App.Models.SoundCloud.User = App.Models.SoundCloud.Base.extend({
   my_activities: function(url, options, next) {
     var that = this;
     SC.get(url, options, function(result) {
-      next( result["next_href"], _.map(result["collection"], function(i) {
-        return new App.Models.SoundCloud.Track(i["origin"], { parse: true });
-      }) );
+      next( result["next_href"], _.compact(_.map(result["collection"], function(i) {
+        if(i["type"] == "track")
+          return new App.Models.SoundCloud.Track(i["origin"], { parse: true });
+        if(i["type"] == "track-sharing")
+          return new App.Models.SoundCloud.Track(i["origin"]["track"], { parse: true });
+      }) ));
     });
   },
   
@@ -30,7 +33,7 @@ App.Models.SoundCloud.User = App.Models.SoundCloud.Base.extend({
     var day_end   = moment().startOf("day").add("days", 1 - days_ago);
     var that = this;
     (function recur(url) {
-      that.my_activities(url, {type: "track"}, function(next_href, data) {
+      that.my_activities(url, {}, function(next_href, data) {
         var oldest_track = _.last(data);
         if(!moment(oldest_track.get("created_at")).isAfter(day_end)) {
           tracks = tracks.concat(_.filter(data, function(i) {
